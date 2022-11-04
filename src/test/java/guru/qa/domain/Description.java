713fb2ccfc189695157c9a649118259955613f42
Artem Kormiltsov <akormilcov@rvision.pro>
2022-11-04 12:29:07 +0300

    add zip file parsing test

---
+++ src/test/java/guru/qa/domain/Description.java
@@ -0,0 +1,41 @@
+package guru.qa.domain;
+
+public class Description {
+    private String name;
+    private String age;
+    private Integer weight;
+    private String[] sports;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public String[] getSports() {
+        return sports;
+    }
+
+    public void setSports(String[] sports) {
+        this.sports = sports;
+    }
+}
